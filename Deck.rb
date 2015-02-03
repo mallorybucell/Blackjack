93c41fcc9a8f9a3e600f0ea524b598917024affd
Mallory Bucell <m.f.bucell@gmail.com>
2015-02-03 05:43:39 -0500

    working deck. Not sure why failing tracking_draw.

--- Deck.rb
+++ Deck.rb
@@ -2,39 +2,40 @@ class Deck
 
   def initialize
     @cards = cards
+    @drawn = []
+
+      
+
+      suit_array = [:C, :D, :H, :S]
+      rank_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      face_array = [:J, :Q, :K, :A]
+      deck_of_cards = []
+      #Create numeric cards
+      suit_array.each do |s|
+        (rank_array.size - 1).times do |r|
+          deck_of_cards << Card.new( rank_array[r].to_i, s.to_sym )
+        end
+      end
+      #Create facecards
+      suit_array.each do |s|
+        face_array.size.times do |r|
+          deck_of_cards << Card.new( face_array[r].to_sym, s.to_sym )
+        end
+      end
+      @cards = deck_of_cards.shuffle!
   end
 
-  def create_deck
-    deck_of_cards = []
-
-    suit_array = [:C, :D, :H, :S]
-    rank_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    face_array = [:J, :Q, :K, :A]
-
-    #Create numeric cards
-    suit_array.each do |s|
-      (rank_array.size - 1).times do |r|
-        deck_of_cards << Card.new( rank_array[r].to_i, s.to_sym )
-      end
-    end
-    suit_array.each do |s|
-      face_array.size.times do |r|
-        deck_of_cards << Card.new( face_array[r].to_sym, s.to_sym )
-      end
-    end
-    
-    return deck_of_cards
-  end
 
   def cards
-    available = create_deck.shuffle
+    @cards
   end
 
   def draw
-    drawn = cards.pop
+    #take = cards.pop
+    @drawn.push(cards.pop) #previously take
   end
 
   def drawn
-    drawn.push(draw)
+    @drawn
   end
 end
